d0a3b7755f0dec3cb98601c335f7e1d6d64bb431
Muhammed Sururat Omotoyosi <sururatomotoyosi@gmail.com>
2025-02-03 09:53:12 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+//! = not
+
+//!true = false
+
+//&& = and
+
+//|| = or
+
+//== = EQUAL
+
+//=== =STRICT EQUAL
+
+let x = 5;
+let y = 5;
+
+let toyosi = x = y;
+let tolu = x * y;
+let tope = x - y;
+let tade = x + y;
+
+console.log(toyosi);
+console.log(tolu);
+console.log(tope);
+console.log(tade);
+
+
+let a = 5;
+let b = 10;
+
+let rahman = a < b;
+console.log(rahman);
+
+console.log(a>b);
+
+console.log(a&&b);
+
+console.log(a||b);
+
+
+
+
+
+
+
+
+
+
+
